7dfa9d401ff33b1846f310c6526106c4b5a9a1d9
cheenwe <cheenwe@gmail.com>
2017-06-26 14:35:00 +0800

    support rails5

--- lib/activemessaging/railtie.rb
+++ lib/activemessaging/railtie.rb
@@ -8,11 +8,20 @@ module ActiveMessaging
     initializer 'activemessaging.initialize' do
       
       ActiveMessaging.load_activemessaging
-      
+
+      # Add a to_prepare block which is executed once in production
+      # and before each request in development
       if defined? Rails
         ActiveMessaging.logger.info "ActiveMessaging: Rails available: Adding dispatcher prepare callback."
-        ActionDispatch::Callbacks.to_prepare do
-          ActiveMessaging.reload_activemessaging
+        case
+          when Gem::Version.new(Rails.version) < Gem::Version.new('3.0.0')
+            ActionDispatch::Callbacks.to_prepare do
+              ActiveMessaging.reload_activemessaging
+            end
+          else
+            config.to_prepare do
+              ActiveMessaging.reload_activemessaging
+            end
         end
       end
 
